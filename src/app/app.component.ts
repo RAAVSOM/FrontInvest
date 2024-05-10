1428f0ceedda1f1464d18c658b5ba40238f8b52b
Raavs <121207361+RAAVSOM@users.noreply.github.com>
2024-05-10 21:20:39 +0000

    noc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OfertasComponent } from './ofertas/ofertas.component';
 import { PerfilOpcionesComponent } from './perfil-opciones/perfil-opciones.component';
+import { CategoriasComponent } from './categorias/categorias.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OfertasComponent, PerfilOpcionesComponent],
+  imports: [
+    RouterOutlet,
+    OfertasComponent,
+    PerfilOpcionesComponent,
+    CategoriasComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'FrontInvest';
--- src/app/categorias/categorias.component.ts
+++ src/app/categorias/categorias.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-categorias',
   standalone: true,
   imports: [],
   templateUrl: './categorias.component.html',
-  styleUrl: './categorias.component.css'
+  styleUrl: './categorias.component.css',
 })
 export class CategoriasComponent {
+  constructor(private route: Router) {}
 
+  mandarLogin(titulo: string): void {
+    this.route.navigateByUrl('/login');
+  }
 }
